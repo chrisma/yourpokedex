568127b2538d807da54ead7bd3fb6a9dc566433a
Christoph Matthies <christoph.matthies@gmail.com>
2016-08-06 14:52:15 +0200

    Extract twitter code into separate class.

--- pokedex.py
+++ pokedex.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from twython import Twython
 from credentials import *
+from tweeter import Tweeter
 from pokedex_data import pokedex
-from pprint import pprint
 from fancy_text import italic, bold
 import itertools
 import random
@@ -14,83 +12,10 @@ import sys
 TWEET_LENGTH = 140
 LENGTH_MEDIA_URL = 24
 MEDIA_TWEET_LENGTH = TWEET_LENGTH - LENGTH_MEDIA_URL
-STEP = 15
 LANGUAGES = ['de','en','es','fr','it','ja','ko','zh']
 PICTURE_PATH_TEMPLATE = 'pokemon-sugimori/{id}.png'
 TWITTER_STATUS_URL_TEMPLATE = 'https://twitter.com/{screen_name}/status/{id}'
 TWITTER_ACCOUNT_NAME = 'yourpokedex'
-
-#
-# TWITTER
-#
-
-def verify_credentials(account):
-	# https://dev.twitter.com/rest/reference/get/account/verify_credentials
-	info = account.verify_credentials(include_entities=False, skip_status=True, include_email=False)
-	name = info.get('name', None)
-	if name is None:
-		logging.error('Could not verify credentials')
-	else:
-		logging.info('Logged in as @{name}, tweets: {tweets}, followers: {followers}'.format(
-			name = name.encode('utf-8'),
-			tweets = info['statuses_count'],
-			followers = info['followers_count']))
-	return info
-
-def upload_twitter_picture(account, picture_path):
-	photo = open(picture_path, 'rb')
-	logging.debug("Uploading '{}'".format(picture_path))
-	response = account.upload_media(media=photo)
-	return response['media_id']
-
-def reply_media_tweet(account, status, reply_id, media_path):
-	media_id = upload_twitter_picture(account, media_path)
-	tweet = account.update_status(status=status, media_ids=[media_id], in_reply_to_status_id=reply_id)
-	logging.info('Responded with media to {}'.format(reply_id))
-	return tweet
-
-def reply_text_tweet(account, status, reply_id):
-	tweet = account.update_status(status=status, in_reply_to_status_id=reply_id)
-	logging.info('Responded with text to {}'.format(reply_id))
-	return tweet
-
-# Find first tweet mentioning any element of query_list_OR
-# in the tweet text (excluding user names).
-# Only tweets for which predicate_func(tweet) is truthy are returned.
-# Returns a tuple of the found status/tweet and what element of
-# the query_list_OR was identified.
-# Returns (None, None) if no matching tweets were found.
-def find_tweet(account, query_list_OR, predicate_func):
-	counter = 0
-	while counter <= len(query_list_OR):
-		current_query = query_list_OR[counter:counter+STEP]
-		logging.debug("Searching for '{}'".format(', '.join(current_query)))
-		statuses = account.search(q=' OR '.join(current_query), count=50)['statuses']
-		rate_limit = account.get_lastfunction_header('x-rate-limit-remaining')
-		logging.info('Rate limit remaining: {}'.format(rate_limit))
-		logging.debug("Found {} matching tweets".format(len(statuses)))
-		counter += STEP
-		for status in statuses:
-			# Should be able to identify which part of the query list was mentioned
-			text = status['text'].lower().encode('utf-8')
-			found = None
-			for query_item in current_query:
-				if text.rfind(query_item.lower()) > -1:
-					found = query_item
-			if found is None:
-				continue
-			# Identified query part should not be part of user's name
-			if status['user']['screen_name'].lower().find(found.lower()) > -1:
-				continue
-			# Identified query part should not be part of a mentioned user's name
-			mentions = status['entities'].get('user_mentions')
-			for m in mentions:
-				if found.lower() in m['screen_name'].lower():
-					continue
-			if not predicate_func(status):
-				continue
-			return (status, found)
-	return (None, None)
 
 #
 # UTILS
@@ -198,12 +123,12 @@ if __name__ == '__main__':
 	logging.getLogger('oauthlib').setLevel(logging.WARN)
 	
 	logging.debug('Started!')
-	account = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	tweeter = Tweeter(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 	random.shuffle(pokedex)
 	poke_names = [p['names']['en'].encode('utf-8') for p in pokedex]
 
-	poke_tweet, pokemon_name = find_tweet(account, poke_names, _should_respond)
+	poke_tweet, pokemon_name = tweeter.find_tweet(poke_names, _should_respond)
 	if poke_tweet is None:
 		logging.warn('No pokemon tweets found :(')
 		sys.exit()
@@ -226,11 +151,10 @@ if __name__ == '__main__':
 
 	if is_media_tweet:
 		picture_path = PICTURE_PATH_TEMPLATE.format(id=pokemon['id'])
-		tweet = reply_media_tweet(account, status, reply_id, picture_path)
+		tweet = tweeter.reply_media_tweet(status, reply_id, picture_path)
 	else:
-		tweet = reply_text_tweet(account, status, reply_id)
+		tweet = tweeter.reply_text_tweet(status, reply_id)
 	logging.info(TWITTER_STATUS_URL_TEMPLATE.format(screen_name=TWITTER_ACCOUNT_NAME, id=tweet['id']))
 	
 	# Tweets that are favorited are not replied to again
-	account.create_favorite(id=reply_id)
-	logging.debug('Favorited tweet {}'.format(reply_id))
+	tweeter.favorite(reply_id)
---
+++ tweeter.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+from twython import Twython
+import logging
+log = logging.getLogger(__name__)
+
+class Tweeter:
+	def __init__(self, app_key, app_secret, oauth_token, oauth_token_secret):
+		self.account = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+		self.step = 15 
+
+	def verify_credentials(self):
+		# https://dev.twitter.com/rest/reference/get/account/verify_credentials
+		info = self.account.verify_credentials(include_entities=False, skip_status=True, include_email=False)
+		name = info.get('name', None)
+		if name is None:
+			log.error('Could not verify credentials')
+		else:
+			log.info('Logged in as @{name}, tweets: {tweets}, followers: {followers}'.format(
+				name = name.encode('utf-8'),
+				tweets = info['statuses_count'],
+				followers = info['followers_count']))
+		return info
+
+	def upload_twitter_picture(self, picture_path):
+		photo = open(picture_path, 'rb')
+		log.debug("Uploading '{}'".format(picture_path))
+		response = self.account.upload_media(media=photo)
+		return response['media_id']
+
+	def reply_media_tweet(self, status, reply_id, media_path):
+		media_id = self.upload_twitter_picture(media_path)
+		tweet = self.account.update_status(status=status, media_ids=[media_id], in_reply_to_status_id=reply_id)
+		log.info('Responded with media to {}'.format(reply_id))
+		return tweet
+
+	def reply_text_tweet(self, status, reply_id):
+		tweet = self.account.update_status(status=status, in_reply_to_status_id=reply_id)
+		log.info('Responded with text to {}'.format(reply_id))
+		return tweet
+
+	def rate_limit_remaining(self):
+		rate_limit = self.account.get_lastfunction_header('x-rate-limit-remaining')
+		log.info('Rate limit remaining: {}'.format(rate_limit))
+		return rate_limit
+
+	def favorite(self, status_id):
+		tweet = self.account.create_favorite(id=status_id)
+		log.debug('Favorited tweet {}'.format(status_id))
+		return tweet
+
+	# Find first tweet mentioning any element of query_list_OR
+	# in the tweet text (excluding user names).
+	# Only tweets for which predicate_func(tweet) is truthy are returned.
+	# Returns a tuple of the found status/tweet and what element of
+	# the query_list_OR was identified.
+	# Returns (None, None) if no matching tweets were found.
+	def find_tweet(self, query_list_OR, predicate_func):
+		counter = 0
+		while counter <= len(query_list_OR):
+			current_query = query_list_OR[counter:counter+self.step]
+			log.debug("Searching for '{}'".format(', '.join(current_query)))
+			statuses = self.account.search(q=' OR '.join(current_query), count=50)['statuses']
+			log.debug("Found {} matching tweets".format(len(statuses)))
+			self.rate_limit_remaining()
+			counter += self.step
+			for status in statuses:
+				# Should be able to identify which part of the query list was mentioned
+				text = status['text'].lower().encode('utf-8')
+				found = None
+				for query_item in current_query:
+					if text.rfind(query_item.lower()) > -1:
+						found = query_item
+				if found is None:
+					continue
+				# Identified query part should not be part of user's name
+				if status['user']['screen_name'].lower().find(found.lower()) > -1:
+					continue
+				# Identified query part should not be part of a mentioned user's name
+				mentions = status['entities'].get('user_mentions')
+				for m in mentions:
+					if found.lower() in m['screen_name'].lower():
+						continue
+				if not predicate_func(status):
+					continue
+				return (status, found)
+		return (None, None)
